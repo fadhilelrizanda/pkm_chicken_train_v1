71938ea8dc83bc430c907aeb9717986dcc0e8b1d
fadhilelrizanda <fadhilelrizanda@gmail.com>
2022-11-17 00:07:59 +0700

    4rd

--- chickenv1/data/change_txt_dataset.py
+++ chickenv1/data/change_txt_dataset.py
@@ -2,9 +2,9 @@ import os
 
 with open('./training.txt', 'w') as out:
     for img in [f for f in os.listdir('./training/train/Chicken') if f.endswith('png')]:
-        out.write('chickenv1/training/train/Chicken/' + img + '\n')
+        out.write('chickenv1/data/training/train/Chicken/' + img + '\n')
 
 
 with open('./testing.txt', 'w') as out:
     for img in [f for f in os.listdir('./training/val/Chicken') if f.endswith('png')]:
-        out.write('chickenv1/training/val/Chicken/' + img + '\n')
+        out.write('chickenv1/data/training/val/Chicken/' + img + '\n')
